b101f5392657f7fbf623f0299c1cb0d6478d10ff
rashm <rashm@RASHU>
2022-07-14 10:22:42 +0530

    first commit

---
+++ corejava/src/corejava_package/p1.java
@@ -0,0 +1,27 @@
+package corejava_package;
+
+public class p1 {
+	int year_ofpurchase;
+	String make;
+	int model;
+	double cost;
+	public void display1()
+	{
+		System.out.println("yeap of purchase"+2001);
+		System.out.println("make"+"suzuki");
+	}
+	public void display2()
+	{
+		System.out.println("model"+1999);
+		System.out.println("cost"+20000);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		p1 ob1=new p1();
+		ob1.display1();
+		ob1.display2();
+
+	}
+
+}
